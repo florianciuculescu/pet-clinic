a83c672ba327bbd1844d77f2f3c3b5dafefadd34
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-23 20:07:42 +0100

    Implemented Interfaces for services

---
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package springframework5.petclinic.services;
+
+import springframework5.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package springframework5.petclinic.services;
+
+import springframework5.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
