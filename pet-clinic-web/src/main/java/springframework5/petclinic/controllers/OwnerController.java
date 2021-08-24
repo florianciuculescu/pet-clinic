4e35a0b7b156d0b717b39ba17018f8990d540a6f
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-24 15:39:56 +0100

    Added owners page and controller

---
+++ pet-clinic-web/src/main/java/springframework5/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package springframework5.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listOfOwners() {
+        return "owners/index";
+    }
+}
