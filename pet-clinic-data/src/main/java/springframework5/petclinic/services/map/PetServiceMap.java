3eac35f0fcc72c91b1b75d7e5272c008c11a3c60
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-24 11:14:48 +0100

    implemented Map based Services

---
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package springframework5.petclinic.services.map;
+
+
+
+import springframework5.petclinic.model.Pet;
+import springframework5.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
