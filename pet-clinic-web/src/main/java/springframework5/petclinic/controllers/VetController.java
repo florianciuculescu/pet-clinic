b2f4c3ec096378ad243b40997e599d3723ebf33d
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-24 15:33:30 +0100

    Added vets page and controller

---
+++ pet-clinic-web/src/main/java/springframework5/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package springframework5.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    public String listVest() {
+
+        return "vets/index";
+    }
+}
