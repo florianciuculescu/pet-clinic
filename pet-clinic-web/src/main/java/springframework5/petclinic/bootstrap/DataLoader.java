b2166c34a9a04b8d2efc221c07e8b45a75e5533a
Florian Ciuculescu <Florian.Ciuculescu@endava.com>
2021-09-15 14:51:38 +0300

    Added pets and Contact info to owners on startup

--- pet-clinic-web/src/main/java/springframework5/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframework5/petclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package springframework5.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springframework5.petclinic.model.Owner;
+import springframework5.petclinic.model.Pet;
 import springframework5.petclinic.model.PetType;
 import springframework5.petclinic.model.Vet;
 import springframework5.petclinic.services.OwnerService;
 import springframework5.petclinic.services.PetTypeService;
 import springframework5.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -38,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
