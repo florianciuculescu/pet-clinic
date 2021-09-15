eaad3c0f4efa043ec88be70f8156363c42a45d9d
Florian Ciuculescu <Florian.Ciuculescu@endava.com>
2021-09-15 15:01:11 +0300

    added specialties map implementation

---
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package springframework5.petclinic.services;
+
+import springframework5.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
