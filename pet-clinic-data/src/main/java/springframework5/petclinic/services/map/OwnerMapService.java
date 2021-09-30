d07cc5a4a9f70005dd690ee4b0bd67a5c21b55b9
fciuculescu <florianciuculescu@yahoo.com>
2021-09-30 17:47:31 +0300

    created spring data jpa service for Specialty refactoring names for map based services

--- pet-clinic-data/src/main/java/springframework5/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/map/OwnerMapService.java
@@ -10,13 +10,13 @@ import springframework5.petclinic.services.PetTypeService;
 import java.util.Set;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService{
 
     private final PetTypeService petTypeService;
     private final PetService petService;
 
 
-    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
         this.petTypeService = petTypeService;
         this.petService = petService;
     }
--- pet-clinic-data/src/main/java/springframework5/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/springframework5/petclinic/services/map/PetMapService.java
@@ -10,7 +10,7 @@ import springframework5.petclinic.services.PetService;
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
