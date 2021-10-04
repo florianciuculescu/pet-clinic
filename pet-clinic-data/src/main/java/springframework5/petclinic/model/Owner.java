209ce9620152d3e677d4b3dec16bed0cfe32dc47
fciuculescu <florianciuculescu@yahoo.com>
2021-10-04 12:51:33 +0300

    Added Lombok to the project and refactored all entities

--- pet-clinic-data/src/main/java/springframework5/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/springframework5/petclinic/model/Owner.java
@@ -1,12 +1,31 @@
 package springframework5.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    /**
+     * adding builder annotation at contructor level in order to access all props from mother classes
+     * This way we can build the full object
+     */
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String address;
@@ -20,35 +39,4 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
--- pet-clinic-data/src/main/java/springframework5/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springframework5/petclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package springframework5.petclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -14,11 +21,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
